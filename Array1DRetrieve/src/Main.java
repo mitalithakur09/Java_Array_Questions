39516707195e217e7cf2ce832a2f6f68d2ae8bc7
Mitali Thakur <103131401+mitalithakur09@users.noreply.github.com>
2023-12-19 18:23:08 +0530

    Add files via upload

---
+++ Array1DRetrieve/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] a = new int[]{10,20,30,40};
+        for (int i = 0; i<a.length;i++){
+            System.out.println(a[i]);
+        }
+
+
+    }
+}
