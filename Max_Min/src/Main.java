81abda948610516003e32a91078b14c061553267
Mitali Thakur <103131401+mitalithakur09@users.noreply.github.com>
2023-12-19 18:25:13 +0530

    Add files via upload

---
+++ Max_Min/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       int[] a = new int[]{90,20,150,40,200};
+       int max = a[0];
+       for(int i = 1; i<a.length; i++){
+           if(a[i]>max){
+               max = a[i];
+           }
+       }
+       System.out.println("max is " + max);
+    }
+}
