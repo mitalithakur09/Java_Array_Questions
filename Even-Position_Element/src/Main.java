bf034a359320b927ccbfe60a3ec1af923ee320f2
Mitali Thakur <103131401+mitalithakur09@users.noreply.github.com>
2024-01-09 16:35:20 +0530

    Add files via upload

---
+++ Even-Position_Element/src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[] a = new int[]{23,45,67,8,9,43,21};
+        for(int i=0; i<a.length; i++){
+            if(i%2==0){
+            System.out.println(a[i]);
+        }
+        }
+    }
+}
